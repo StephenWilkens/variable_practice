30442be0d7195c1b9cd19bc8a01e96367ba1406a
Stephen Wilkens <stephenwilkens@gmail.com>
2022-02-07 20:27:51 -0700

    add annotation

--- variables.rb
+++ variables.rb
@@ -1,11 +1,11 @@
 hometown = "Grass Valley"
 highschool = "HBHS"
 first_pet = "Sherman"
-
+# creating the variable "age" and assigning it to the integer 29
 age = 29
 weight = 160
 digits = 20
-
+#creating the variable account_balance and assigning it to the float 2000.00
 account_balance = -20000.00
 favorite_time = 12.05
 shoe_size = 9.5
@@ -13,7 +13,7 @@ shoe_size = 9.5
 red_head = true
 broke_as_a_joke = true
 bad_at_bikes = false
-
+# reassigning the variable hometown to the string Huntington Beach
 hometown = "Huntington Beach"
 highschool = "drop out"
 first_pet = "turtle"
